9e5fc54b4b825eb772dab50e75b210276d4b87f0
unknown <piotrflorczakk@gmail.com>
2020-09-14 18:52:41 +0200

    Literówka w Stronie 404

--- 404.php
+++ 404.php
@@ -13,7 +13,7 @@ get_header();
 	<div class="wp-block-group bitad-404 bitad-center">
 		<div class="wp-block-group__inner-container">
 			<h1 class="has-text-align-center">Coś poszło nie tak...</h1>
-			<p class="has-text-align-center bitad-hero-subtitle">Strona której szukasz nie istnieje. Sprawdź adres URL po względem literówek. Jeśli nadal masz problem to spróbuj wrócić na stronę główną konferencji.</p>
+			<p class="has-text-align-center bitad-hero-subtitle">Strona której szukasz nie istnieje. Sprawdź adres URL pod względem literówek. Jeśli nadal masz problem to spróbuj wrócić na stronę główną konferencji.</p>
 			<div class="wp-block-buttons aligncenter">
 				<div class="wp-block-button bitad-hero-button is-style-outline"><a class="wp-block-button__link has-text-color" href="/" style="border-radius:16px;color:#4093cd"><strong>Strona Główna</strong></a></div>
 			</div>
